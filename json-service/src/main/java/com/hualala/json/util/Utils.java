14bb1a74d7066c2c8a2c776e1ea90c1639deb8ee
huzhe <huzhe@hulala.com>
2018-05-22 11:38:04 +0800

    修改方法名

--- json-service/src/main/java/com/hualala/json/util/Utils.java
+++ json-service/src/main/java/com/hualala/json/util/Utils.java
@@ -6,13 +6,13 @@ import net.sf.json.JSONObject;
 
 public class Utils {
 
-    public static <T> Object transformJsonToBean(String json, Class<T> type) {
+    public static <T> Object transfromJsonToBean(String json, Class<T> type) {
         JSONObject jsonObject = JSONObject.fromObject(json);
         return JSONObject.toBean(jsonObject, type);
     }
 
     //此方法比DataUtil.parseJsonToBeanList方法优
-    public static <T> Object transformJsonToBeanList(String jsonArr,Class<T> type) {
+    public static <T> Object transfromJsonToBeanList(String jsonArr,Class<T> type) {
         JSONArray jsonArray = JSONArray.fromObject(jsonArr);
         return JSONArray.toCollection(jsonArray, type);
     }
